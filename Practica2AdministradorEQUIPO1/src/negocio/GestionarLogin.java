ee95e085fdd676bc2da2b504e924e8d7a8e19dbd
RodAvl <rogelio.bcngr@gmail.com>
2016-03-08 23:35:20 -0600

    Se modifica el constructor por parametros; recibiendo un objeto ConexionMySQL

--- Practica2AdministradorEQUIPO1/src/negocio/GestionarLogin.java
+++ Practica2AdministradorEQUIPO1/src/negocio/GestionarLogin.java
@@ -15,23 +15,27 @@ public class GestionarLogin {
 	private Statement st=null;
 	private ResultSet rs = null;
 	
-	public GestionarLogin() {
+	public GestionarLogin(){
+		
+	}
+	
+	public GestionarLogin(ConexionMySQL conx) {
 		unUsuario = new Usuario();
-		nuevaConexion = new ConexionMySQL();
-		connection = nuevaConexion.obtenerConexion();
+		nuevaConexion = conx;
+		connection = conx.obtenerConexion();
 	}
 	
 //	Busca al usuario correspondiente para el login
 	public Usuario buscarUsuario(String correo,String contrasenia) {
-		String consulta = "SELECT id, nombre, correo,contrasenia, idTipoUsuario  FROM usuarios where correo='"+correo+"' AND constrasenia='"+contrasenia+"'";
+		String consulta = "SELECT id, nombre, correo,contrasenia, idTipoUsuario  FROM usuarios where correo='"+correo+"' AND contrasenia='"+contrasenia+"'";
 		try {
+			st = connection.createStatement();
 //			Un resulset contiene todas las filas que satisface las con
 			rs= st.executeQuery(consulta);
 //			el metodo resultset.next se usa para mover a la siguiente fila , convirtiendola en la fila actual
 			while(rs.next()){
 				if((rs.getString("correo").equals(correo)) && (rs.getString("contrasenia").equals(contrasenia))){
 					System.out.println("Usuario encontrado");
-					unUsuario.setIdUsuario();
 					unUsuario.setNombreCompleto(rs.getString("nombre"));
 					unUsuario.setCorreo(rs.getString("correo"));
 					unUsuario.setContrasenia(rs.getString("contrasenia"));
